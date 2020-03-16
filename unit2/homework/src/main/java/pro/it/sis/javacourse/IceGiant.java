f8f77a257c442bcc1f6ab9b4a4ec1bfb8edf1aa6
buryad <41693482+buryad@users.noreply.github.com>
2020-03-16 23:28:29 +0700

    battle homework2 update

--- unit2/homework/src/main/java/pro/it/sis/javacourse/IceGiant.java
+++ unit2/homework/src/main/java/pro/it/sis/javacourse/IceGiant.java
@@ -1,5 +1,5 @@
 package pro.it.sis.javacourse;
-// Ледяной великан - иммунитет к огню
+// Ледяной великан - иммунитет к холоду
 public class IceGiant extends Target {
     public IceGiant() {
         super(100,50,0);
--- unit2/homework/src/main/java/pro/it/sis/javacourse/Main.java
+++ unit2/homework/src/main/java/pro/it/sis/javacourse/Main.java
@@ -2,9 +2,9 @@ package pro.it.sis.javacourse;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем объект Меч Ночи в Якутске
+        // Создаем объект Меч Ночи в Якутске, бьет холодом 50
         Weapon frozenSword = new FrozenSword(new Damage(0,0,50));
-        // Создаем объект Меч Пылающий асфальт
+        // Создаем объект Меч Пылающий асфальт, бьет огнем 50
         Weapon fireSword = new FireSword(new Damage(0,50,0));
         Target iceGiant = new IceGiant();
         Target efreet = new Efreet();
