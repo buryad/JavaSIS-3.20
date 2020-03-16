d9740ac3bd4ec05b7e26e2e97629961fd199426a
buryad <41693482+buryad@users.noreply.github.com>
2020-03-16 13:28:45 +0700

    battle homework2

--- unit2/homework/src/main/java/pro/it/sis/javacourse/Target.java
+++ unit2/homework/src/main/java/pro/it/sis/javacourse/Target.java
@@ -1,22 +1,33 @@
 package pro.it.sis.javacourse;
 
-public class Target {
-
-    public int getPhysicalDamage() {
-        return physicalDamage;
+public abstract class Target {
+    private double health;
+    private double iceResist;
+    private double fireResist;
+    protected Target(double health, double iceResist, double fireResist) {
+        this.health = health;
+        this.iceResist = iceResist;
+        this.fireResist = fireResist;
     }
-
-    public int getFireDamage() {
-        return fireDamage;
+    public double getHealth() {
+        return health;
     }
-
-    public int getIceDamage() {
-        return iceDamage;
+    public double getIceResist() {
+        return iceResist;
     }
-
-    private int physicalDamage;
-
-    private int fireDamage;
-
-    private int iceDamage;
+    public double getFireResist() {
+        return fireResist;
+    }
+    public void takeDamage(Weapon weapon){
+        System.out.println("Здоровье до удара: " + this.health);
+        if (fireResist > 0){
+            // из здоровья вычитаем физ урон, урон от льда и в два раза меньше урон от огня
+            health = health - weapon.getDamage().getPhysicalDamage() - weapon.getDamage().getIceDamage() - weapon.getDamage().getFireDamage()/2.0;
+        }
+        if (iceResist > 0) {
+            // из здоровья вычитаем физ урон, в два раза меньше урон от льда и урон от огня
+            health = health - weapon.getDamage().getPhysicalDamage() - weapon.getDamage().getIceDamage() / 2.0 - weapon.getDamage().getFireDamage();
+        }
+        System.out.println("Здоровье после удара: " + this.health);
+    }
 }
